9139b04be349c3353dfcde8ab0627be1a3b01a0f
AlanLucasSC <AlanLucasSC2011@gmail.com>
2019-02-14 14:58:58 -0300

    Updates

---
+++ precache-manifest.2d06015e9dbf7bdea2c4ae228324d209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "212f23e40670f3dce930",
+    "url": "/PWA-QrCode/static/css/main.91177226.chunk.css"
+  },
+  {
+    "revision": "212f23e40670f3dce930",
+    "url": "/PWA-QrCode/static/js/main.212f23e4.chunk.js"
+  },
+  {
+    "revision": "b3e25f71edae61775ecc",
+    "url": "/PWA-QrCode/static/js/runtime~main.b3e25f71.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/PWA-QrCode/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PWA-QrCode/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b76b741543311f0b245c8833bcb106bc",
+    "url": "/PWA-QrCode/index.html"
+  }
+];
